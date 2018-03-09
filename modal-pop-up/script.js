851e7d0f0ed080324a0c459f726dd690a7d6bc08
Parvez Shaikh <parvez.shaikh@bookmyshow.com>
2018-03-09 16:28:24 +0530

    Modal Pop Up Updated

---
+++ modal-pop-up/script.js
@@ -0,0 +1,15 @@
+document
+  .getElementById('button')
+  .addEventListener('click', function(){
+    document
+      .querySelector('.bg-modal')
+      .style.display = 'flex';
+});
+
+document
+  .querySelector('.close')
+  .addEventListener('click', function(){
+  document
+      .querySelector('.bg-modal')
+      .style.display = 'none';
+})
